16dd574cf4c2e8ed06e17efd5b4417d35e52611a
Atif <iamatif96@gmail.com>
2024-08-19 07:04:37 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int n=2, m=10;
+        for(int i=0; i<n; i++){
+            System.out.println("print i: "+i);
+            for(int j=0; j<m; j++){
+                System.out.println("print j: "+j);
+            }
+        }
+    }
+}
